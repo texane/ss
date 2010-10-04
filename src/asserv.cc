9a7a90e99560f6f930139215716dd23fd897f71b
Le Mentec Fabien <fabien.lementec@imag.fr>
2010-10-04 09:42:13 -0500

    [wip]

--- src/asserv.cc
+++ src/asserv.cc
@@ -1,31 +1,84 @@
 #include "asserv.hh"
 
 
+// private
+
+void asserv::set_cmd(enum cmd_op op, double a, double b, double c)
+{
+  _cmd._op = op;
+
+  _cmd._args[0] = a;
+  _cmd._args[1] = b;
+  _cmd._args[2] = c;
+}
+
+
+// exported
+
+asserv::asserv()
+{
+  set_cmd(CMD_OP_NONE);
+}
+
+
 void asserv::move_forward(double d)
 {
+  set_cmd(CMD_OP_MOVE_FORWARD, d);
 }
 
 
 void asserv::rotate(double a)
 {
+  set_cmd(CMD_OP_ROTATE, a);
 }
 
 
 void asserv::set_pos(double x, double y)
 {
+  _bot->_phys->set_pos(x, y);
 }
 
 
 void asserv::get_pos(double& x, double& y)
 {
+  _bot->_phys->get_pos(x, y);
+}
+
+
+void asserv::set_power(bool ison)
+{
+  if (ison == false)
+    _bot->_phys->set_vel(0);
 }
 
 
 bool asserv::is_done()
 {
-  & phys = asserv->_bot->_phys_context;
-
-  if (_last_cmd == && phys->_pos == pos)
+  if (_cmd._op == NONE)
     return true;
-  asserv->_bot->phys ;
+  return false;
+}
+
+
+void asserv::next()
+{
+  switch (_cmd._op)
+  {
+  case CMD_OP_MOVE_FORWARD:
+    double x, y;
+    _bot->_phys->get_pos(x, y);
+    if ((x == _cmd._args[0]) && (y == _cmd._args[1]))
+      _cmd._op = CMD_OP_NONE;
+    break;
+
+  case CMD_OP_ROTATE:
+    double a;
+    _bot->_phys->get_angle(a);
+    if (a == _cmd._args[0])
+      _cmd._op = CMD_OP_NONE;
+    break;
+
+  default:
+    break;
+  }
 }
--- src/asserv.hh
+++ src/asserv.hh
@@ -2,14 +2,46 @@
 # define ASSERV_HH_INCLUDED
 
 
+// forward decl
+class bot;
+
+
 class asserv
 {
+  // physics engine backlink
+  bot* _bot;
+
+  // last issued command
+
+  enum cmd_op
+  {
+    CMD_OP_NONE = 0,
+    CMD_OP_MOVE_FORWARD,
+    CMD_OP_ROTATE,
+    CMD_OP_INVALID
+  };
+
+  typedef struct cmd
+  {
+    enum cmd_op _op;
+    double _args[3];
+  } cmd_t;
+
+  cmd_t _cmd;
+
+  void set_cmd(enum cmd_op, double = 0.f, double = 0.f, double = 0.f);
+
 public:
+  asserv();
+
   void move_forward(double);
   void rotate(double);
   void set_pos(double, double);
   void get_pos(double, double);
+  void set_power(bool);
   bool is_done();
+
+  void next();
 };
 
 
